918ab88364738264533618956e9f1042e0ee5570
priscila-conmits <dominguezpriscila2005@gmail.com>
2026-05-04 21:40:10 -0300

    Create calculadora.py
    git add calculadora.py
    git commit -m "Crear calculadora con funcion de suma"
    git push

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+# calculadora.py
+def sumar(a, b):
+  return a + b
+
+# codigo principal
+if _name_ == "_main_":
+  print("=== Calculadora colaborativa ===")
+  num1 = int(input("Ingrese primer numero: "))
+  num2 = int(input("Ingrese segundo numero: "))
+  print("Resultado suma:", sumar(num1, num2))
+  
